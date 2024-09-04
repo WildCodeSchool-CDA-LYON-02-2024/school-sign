5604fc0520e3eed7b8bf5115846cf6c3b3d22744
Messaoud ZOUGGAB <messaoud.zouggab@gmail.com>
2024-09-04 16:43:04 +0200

    test

--- lib/schemas/registerSchema.ts
+++ lib/schemas/registerSchema.ts
@@ -30,25 +30,7 @@ export const registerSchema = z
     //       "La confirmation du mot de passe doit comporter au maximum 255 caractères.",
     //   }),
   })
-<<<<<<< HEAD
-  .refine((data) => data.password === data.confirmPassword, {
-    path: ["confirmPassword"],
-    message: "Les mots de passe ne correspondent pas.",
-  });
-
-export type RegisterSchema = z.infer<typeof registerSchema>;
-
-export const loginSchema = z.object({
-  email: z.string().email({ message: "L'email doit être valide." }).max(255, {
-    message: "L'email doit comporter au maximum 255 caractères.",
-  }),
-  password: z.string().min(8, {
-    message: "Le mot de passe doit comporter au moins 8 caractères.",
-  }),
-});
-=======
   // .refine((data) => data.password === data.confirmPassword, {
   //   path: ["confirmPassword"],
   //   message: "Les mots de passe ne correspondent pas.",
   // });
->>>>>>> d0b66f192f25ae89b46c8f6d74330ab5f3806167
