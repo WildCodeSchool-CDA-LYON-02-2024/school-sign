bbff406111b6408a90d5d4ad76bd6da5ff52083b
Pat <hoang.patrick@outlook.com>
2024-09-11 12:33:25 +0200

    ajout params pour le student by id

--- app/(school)/school-dashboard/class/[name]/student/[id]/page.tsx
+++ app/(school)/school-dashboard/class/[name]/student/[id]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-export default function StudentById() {
+export default function StudentById({ params }: { params: { id: string } }) {
   return (
     <div>
       <div className="flex items-center justify-center h-screen flex-col gap-4 p-4 md:p-36">
-        <h1>Ajouter les informations d&apos;un student via son ID</h1>
+        <h1>
+          Ajouter les informations d&apos;un student via son ID: {params.id}
+        </h1>
       </div>
     </div>
   );
