853c71f3942d64cda370d8fdb65da4633d7c50c1
Patrick Hoang <hoang.patrick@outlook.com>
2024-09-19 12:04:45 +0200

    ok

--- app/(teacher)/teacher-dashboard/page.tsx
+++ app/(teacher)/teacher-dashboard/page.tsx
@@ -3,17 +3,44 @@
 import { useEffect, useState } from "react";
 import { useSignatureContext } from "../../../components/context/SignatureContext";
 import Image from "next/image";
+import { DataTable } from "@/components/table/data-table";
+import { columns, Payment } from "@/components/table/columns";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "489e1d42",
+      amount: 125,
+      status: "processing",
+      email: "example@gmail.com",
+    },
+    // ...
+  ];
+}
 
 export default function TeacherDashboard() {
+  const [data, setData] = useState<Payment[]>([]);
+  const [classId, setClassId] = useState<number | null>(null);
   const {
     allowSignature,
     disallowSignature,
     isSignatureAllowed,
-    studentSignatures, // Utiliser studentSignatures
+    studentSignatures,
   } = useSignatureContext();
-  const [classId, setClassId] = useState<number | null>(null);
 
   useEffect(() => {
+    const fetchData = async () => {
+      const fetchedData = await getData();
+      setData(fetchedData);
+    };
+
     const fetchClassId = async () => {
       const response = await fetch("/api/getClassIdByToken");
       if (response.ok) {
@@ -24,6 +51,7 @@ export default function TeacherDashboard() {
       }
     };
 
+    fetchData();
     fetchClassId();
   }, []);
 
@@ -38,6 +66,9 @@ export default function TeacherDashboard() {
   return (
     <div className="h-screen py-8 w-full">
       <h1 className="text-center">Teacher Dashboard</h1>
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
       {classId ? (
         <div className="container mx-auto py-10">
           <button
@@ -70,7 +101,7 @@ export default function TeacherDashboard() {
                       alt={`Signature de l'élève ${index + 1}`}
                       width={600}
                       height={500}
-                    ></Image>
+                    />
                   </div>
                 ))}
               </div>
