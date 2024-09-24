4f776364888e1a0d73c48b312ae1d74600d880fc
Johana Lavigne <johana.lavigne@gmail.com>
2024-09-24 11:16:05 +0200

    feat: tanstack columns def

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -1,27 +1,30 @@
 "use client";
 
-import { ColumnDef } from "@tanstack/react-table"; // This type is used to define the shape of our data.
+import { ColumnDef } from "@tanstack/react-table";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
 export type Payment = {
   id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
+  lastname: string;
+  firstname: string;
   email: string;
+  signature: "pending" | "processing" | "success" | "failed";
 };
 
 export const columns: ColumnDef<Payment>[] = [
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "lastname",
+    header: "Lastname",
+  },
+  {
+    accessorKey: "firstname",
+    header: "Firstname",
   },
   {
     accessorKey: "email",
     header: "Email",
   },
   {
-    accessorKey: "amount",
-    header: "Amount",
+    accessorKey: "signature",
+    header: "Signature",
   },
 ];
