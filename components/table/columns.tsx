e5cc1845d94d0a9fa0b335e772054b09afc8467b
Johana Lavigne <johana.lavigne@gmail.com>
2024-09-24 11:16:30 +0200

    feat: tanstack columns type

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -2,7 +2,7 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 
-export type Payment = {
+export type Class = {
   id: string;
   lastname: string;
   firstname: string;
@@ -10,7 +10,7 @@ export type Payment = {
   signature: "pending" | "processing" | "success" | "failed";
 };
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<Class>[] = [
   {
     accessorKey: "lastname",
     header: "Lastname",
