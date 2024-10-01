cef96c3dd769f8eb26b484a65f7da09aac550ce9
Johana Lavigne <johana.lavigne@gmail.com>
2024-10-01 11:49:04 +0200

    fix: ui

--- app/(student)/student-dashboard/schedule/page.tsx
+++ app/(student)/student-dashboard/schedule/page.tsx
@@ -2,12 +2,20 @@
 
 // components
 import CalendarTest from "@/components/calendar/CalendarTest";
+import { Separator } from "@/components/ui/separator";
 
 export default function StudentSchedule() {
   return (
     <>
-      <h1 className="text-2xl font-bold tracking-tight">Schedule</h1>
-      <CalendarTest />
+      <div className="space-y-6 px-10 lg:px-0 pb-16 md:block">
+        <div className="space-y-0.5">
+          <h1 className="text-2xl font-bold tracking-tight">Schedule</h1>
+        </div>
+        <Separator />
+        <div className="w-full pt-6 md:pr-10 lg:pr-24">
+          <CalendarTest />
+        </div>
+      </div>
     </>
   );
 }
