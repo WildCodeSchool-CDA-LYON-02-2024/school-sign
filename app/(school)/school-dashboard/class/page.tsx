62fde7f633d1709861ff3807bfca8931fa3b798f
Johana Lavigne <johana.lavigne@gmail.com>
2024-10-01 11:35:52 +0200

    fix: large screen size

--- app/(school)/school-dashboard/class/page.tsx
+++ app/(school)/school-dashboard/class/page.tsx
@@ -59,7 +59,7 @@ export default function ClassList() {
 
   return (
     <>
-      <div className="space-y-6 px-10 pb-16 md:block">
+      <div className="space-y-6 px-10 lg:px-0 pb-16 md:block">
         <div className="space-y-0.5">
           <h1 className="text-2xl font-bold tracking-tight">Classes</h1>
           <p className="text-muted-foreground">Consult School Classes.</p>
--- app/(school)/school-dashboard/documents/page.tsx
+++ app/(school)/school-dashboard/documents/page.tsx
@@ -4,7 +4,7 @@ import { Separator } from "@/components/ui/separator";
 export default function Docs() {
   return (
     <>
-      <div className="space-y-6 px-10 pb-16 md:block">
+      <div className="space-y-6 px-10 lg:px-0 pb-16 md:block">
         <div className="space-y-0.5">
           <h1 className="text-2xl font-bold tracking-tight">Attendance</h1>
           <p className="text-muted-foreground">Generate an attendance sheet.</p>
